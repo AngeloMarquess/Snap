bf2eb1fff3f14c640591048c99dd71607f4048c4
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2022-10-15 17:28:34 -0300

    feat(submenu): adicionado o submenu, restando implementar o js

---
+++ script.js
@@ -0,0 +1,28 @@
+// const arrow = document.getElementById('arrow');
+// const menu = document.getElementById('menu');
+
+// function isOpen() {
+//   if (menu.classList.contains('open')) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+// info.addEventListener('click', () => {
+//   if (isOpen()) {
+//     hideMenu();
+//   } else {
+//     showMenu();
+//   };
+// });
+
+// function hideMenu() {
+//   menu.classList.remove('open');
+//   menu.classList.add('hide');
+// };
+
+// function showMenu() {
+//   menu.classList.add('open');
+//   menu.classList.remove('hide');
+// };
