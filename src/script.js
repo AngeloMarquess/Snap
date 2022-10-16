8b50ae7ae76d876bafa82c394351894261c1ea07
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2022-10-16 13:30:46 -0300

    refactor(script.js): feita a refatoração das funções do submenu

--- src/script.js
+++ src/script.js
@@ -3,54 +3,55 @@ const linkComp = document.getElementsByClassName('arrow')[1];
 const menuFeat = document.querySelector('.menu-feat');
 const menuComp = document.querySelector('.menu-comp');
 
-function featIsOpen() {
-  if (menuFeat.classList.contains('open')) {
-    return true;
-  } else {
-    return false;
+const menuOnCLick = {
+  linkFeat: linkFeat.addEventListener('click', () => {
+    if (checkIsOpen.feat()) {
+      hideMenus.feat();
+    } else {
+      showMenus.feat();
+    };
+  }),
+  linkComp: linkComp.addEventListener('click', () => {
+    if (checkIsOpen.comp()) {
+      hideMenus.comp();
+    } else {
+      showMenus.comp();
+    };
+  }),
+}
+const checkIsOpen = {
+  feat: function featIsOpen() {
+    if (menuFeat.classList.contains('open')) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  comp: function compIsOpen() {
+    if (menuComp.classList.contains('open')) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+};
+const hideMenus = {
+  feat: function hideFeatMenu() {
+    menuFeat.classList.remove('open');
+    menuFeat.classList.add('hide');
+  },
+  comp: function hideCompMenu() {
+    menuComp.classList.remove('open');
+    menuComp.classList.add('hide');
+  },
+};
+const showMenus = {
+  feat: function showFeatMenu() {
+    menuFeat.classList.add('open');
+    menuFeat.classList.remove('hide');
+  },
+  comp: function showCompMenu() {
+    menuComp.classList.add('open');
+    menuComp.classList.remove('hide');
   }
-};
-
-function compIsOpen() {
-  if (menuComp.classList.contains('open')) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-linkFeat.addEventListener('click', () => {
-  if (featIsOpen()) {
-    hideFeatMenu();
-  } else {
-    showFeatMenu();
-  };
-});
-
-linkComp.addEventListener('click', () => {
-  if (compIsOpen()) {
-    hideCompMenu();
-  } else {
-    showCompMenu();
-  };
-});
-
-function hideCompMenu() {
-  menuComp.classList.remove('open');
-  menuComp.classList.add('hide');
-};
-
-function showCompMenu() {
-  menuComp.classList.add('open');
-  menuComp.classList.remove('hide');
-};
-
-function hideFeatMenu() {
-  menuFeat.classList.remove('open');
-  menuFeat.classList.add('hide');
-};
-
-function showFeatMenu() {
-  menuFeat.classList.add('open');
-  menuFeat.classList.remove('hide');
 };
